26507e7a0eb346f1cc9bcb8e12a2a221bd10e9ab
Reint Jan Hoiting <reintjanhoiting@hotmail.com>
2012-05-30 14:34:33 +0200

    Setting SingleTon

--- OML_App/Setting/Settings_Singleton.cs
+++ OML_App/Setting/Settings_Singleton.cs
@@ -9,6 +9,8 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using OML_App.Data;
+using OML_App.net.ukct.reintjan1;
 
 namespace OML_App.Setting
 {
@@ -17,6 +19,14 @@ namespace OML_App.Setting
         private static volatile Settings_Singleton instance;
         private static object syncRoot = new Object();
 
+        public List<OML_App.Data.Session> _Session = new List<OML_App.Data.Session>();
+        public int Thershold;//Thershold Rate TCP
+        public int UpdateRate;//Update Rate TCP
+        public int IpAdress; //Default Ip , CurrentSession
+        public int Port; //Default port, CurrentSession
+        public bool Carmen = false; //True if Besturen eigen auto
+
+        public WebService Ws = new WebService();
 
         public static Settings_Singleton Instance
         {
@@ -34,6 +44,40 @@ namespace OML_App.Setting
                     }
                 }
                 return instance;
+            }
+        }
+
+        /// <summary>
+        /// Sends a Session to the Webservice! It will be saved over there
+        /// </summary>
+        /// <param name="ses"></param>
+        public void SessionToWeb(OML_App.Data.Session ses) 
+        {
+            Ws.SaveNewSession((OML_App.net.ukct.reintjan1.Session)ses);
+        }
+
+        /// <summary>
+        /// Get a #sessions in a List of Sessions from the WebService! Keep in mind there will be L@gg!!
+        /// </summary>
+        /// <param name="number">#numbers</param>
+        public void GetList(int number)
+        {
+            OML_App.net.ukct.reintjan1.Session[] _ses = Ws.GetSessionList(number).ToArray<OML_App.net.ukct.reintjan1.Session>();
+            foreach (OML_App.net.ukct.reintjan1.Session ses in _ses)
+            {
+                _Session.Add((OML_App.Data.Session)ses);
+            }
+        }
+
+        /// <summary>
+        /// Get a List of Sessions of all the Sessions from the WebService! Keep in mind there will be L@gg!!
+        /// </summary>
+        public void GetAllList()
+        {
+            OML_App.net.ukct.reintjan1.Session[] _ses = Ws.GetAllSession().ToArray<OML_App.net.ukct.reintjan1.Session>();
+            foreach (OML_App.net.ukct.reintjan1.Session ses in _ses)
+            {
+                _Session.Add((OML_App.Data.Session)ses);
             }
         }
     }
